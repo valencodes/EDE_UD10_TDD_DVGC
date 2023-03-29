7c73d9c64f6db78de7143dc253cbc21e93928721
Valentina <valentinagaleote@gmail.com>
2023-03-29 20:42:37 +0200

    Refactorizados nombres de los métodos de la clase Coche añadiendo mi nombre al final.

--- src/main/java/Coche.java
+++ src/main/java/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_Daniela_Valentina_Galeote_Calderon(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_Daniela_Valentina_Galeote_Calderon(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad <0) velocidad = 0;
     }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -10,7 +10,7 @@ public class TestCoche {
     @Test
     public void test_al_acelerar_un_coche_su_velocidad_aumenta_Daniela_Valentina_Galeote_Calderon(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_Daniela_Valentina_Galeote_Calderon(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
@@ -18,7 +18,7 @@ public class TestCoche {
     public void test_al_decelerar_un_coche_su_velocidad_disminuye_Daniela_Valentina_Galeote_Calderon(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerar_Daniela_Valentina_Galeote_Calderon(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
@@ -26,7 +26,7 @@ public class TestCoche {
     public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_Daniela_Valentina_Galeote_Calderon(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_Daniela_Valentina_Galeote_Calderon(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
